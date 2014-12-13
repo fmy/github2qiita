adea76d2608033ad60a064f2c9734c894c5be5d4
Fumiya KARASAWA <krswfmy@gmail.com>
2014-12-13 15:18:22 +0900

    bugfix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 		user = User.find_by_provider_and_uid(auth['provider'], auth['uid']) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     user, repo = session[:repo_name].split('/')
-    redirect_to "list?user=#{user}&repo=#{repo}"
+    redirect_to "/list?user=#{user}&repo=#{repo}"
 	end
 
 	def destroy
